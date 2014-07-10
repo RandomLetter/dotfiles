ae413afec32386eb4a28f62a41f5f498aac2292c
rohit <rdmltrs@gmail.com>
2014-07-10 13:06:19 -0400

    Add ELPA package directory; updated init.el with MELPA and solarized-dark color theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,6 +27,11 @@
 (setq show-trailing-whitespace t)
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -34,20 +39,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(column-marker-1 ((t (:background "red"))) t)
+ '(diff-added ((t (:foreground "cyan"))) t)
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))) t)
  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
+ '(fundamental-mode-default ((t (:inherit default))) t)
  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
  '(linum ((t (:foreground "black" :weight bold))))
  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
-)
+ '(show-paren-match ((((class color) (background light)) (:background "black"))) t)
+ '(vertical-border ((t nil))))
 
+(load-theme 'solarized-dark t)
 ;; ------------
 ;; -- Macros --
 ;; ------------
